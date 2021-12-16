e7f247d8823e630f37ea39180ce796d3deaa55f0
Gabriel Munhoz Nóbrega <gmnobrega0704@gmail.com>
2021-12-16 16:28:20 -0300

    Updates project rules

--- electron/bridge.ts
+++ electron/bridge.ts
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron'
+import { contextBridge, ipcRenderer } from "electron"
 
 export const api = {
   /**
@@ -10,7 +10,7 @@ export const api = {
    */
 
   sendMessage: (message: string) => {
-    ipcRenderer.send('message', message)
+    ipcRenderer.send("message", message)
   },
 
   /**
@@ -18,7 +18,7 @@ export const api = {
    */
   on: (channel: string, callback: Function) => {
     ipcRenderer.on(channel, (_, data) => callback(data))
-  }
+  },
 }
 
-contextBridge.exposeInMainWorld('Main', api)
+contextBridge.exposeInMainWorld("Main", api)
